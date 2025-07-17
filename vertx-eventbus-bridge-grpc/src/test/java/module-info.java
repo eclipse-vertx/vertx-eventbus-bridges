b3d1ed28020d1cdbba61ab4c1b22b6f2156a63fe
Julien Viet <julien@julienviet.com>
2025-07-17 15:45:05 +0200

    Module code format

--- vertx-eventbus-bridge-grpc/src/test/java/module-info.java
+++ vertx-eventbus-bridge-grpc/src/test/java/module-info.java
@@ -1,13 +1,11 @@
 open module io.vertx.tests.eventbusbridge.grpc {
-    requires io.vertx.eventbusbridge.common;
+  requires io.vertx.eventbusbridge.common;
   requires io.vertx.eventbusbridge.grpc;
-
   requires io.vertx.grpc.client;
-
-    requires junit;
+  requires junit;
   requires assertj.core;
   requires io.vertx.testing.unit;
-    requires io.vertx.grpc.server;
+  requires io.vertx.grpc.server;
   requires io.vertx.grpc.common;
   requires io.vertx.core;
   requires com.google.protobuf;
